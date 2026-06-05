507b6c79de01d5bbb6e34c14df011a0647e30719
Daniel Hwang <daniel@ines.local>
2026-06-04 19:52:55 -0500

    Initial flakes

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;}
+    {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = ["x86_64-linux"];
+      perSystem = {pkgs, ...}: {
+        devshells.default = {
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+            gofumpt
+          ];
+        };
+
+        formatter = pkgs.alejandra;
+      };
+    };
+}
